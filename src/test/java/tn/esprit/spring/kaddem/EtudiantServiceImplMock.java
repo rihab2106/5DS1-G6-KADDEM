d218d50a598d224dac8b88644189948d0db0eed8
Ayoub <ayoub.mouelhi@esprit.tn>
2023-11-03 21:56:00 +0100

    Mockito class test ayoub etudiant

---
+++ src/test/java/tn/esprit/spring/kaddem/EtudiantServiceImplMock.java
@@ -0,0 +1,63 @@
+package tn.esprit.spring.kaddem;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
+import tn.esprit.spring.kaddem.services.EtudiantServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class EtudiantServiceImplMock {
+
+    @InjectMocks
+    private EtudiantServiceImpl etudiantService;
+
+    @Mock
+    private EtudiantRepository etudiantRepository;
+
+    @Test
+    public void testAddEtudiant() {
+        // Mock the behavior of the EtudiantRepository for save method
+        Etudiant etudiant = new Etudiant();
+        etudiant.setNomE("Jane");
+        etudiant.setPrenomE("Smith");
+
+        Mockito.when(etudiantRepository.save(Mockito.any(Etudiant.class))).thenReturn(etudiant);
+
+        Etudiant addedEtudiant = etudiantService.addEtudiant(etudiant);
+
+        // Now, you should assert the values of the addedEtudiant object
+        assertEquals("Jane", addedEtudiant.getNomE());
+        assertEquals("Smith", addedEtudiant.getPrenomE());
+    }
+
+    @Test
+    public void testRetrieveAllEtudiants() {
+        // Mock the behavior of the EtudiantRepository for findAll method
+        Etudiant etudiant = new Etudiant();
+        etudiant.setIdEtudiant(1);
+        etudiant.setNomE("John");
+        etudiant.setPrenomE("Doe");
+
+        List<Etudiant> etudiantList = new ArrayList<>();
+        etudiantList.add(etudiant);
+
+
+
+        Mockito.when(etudiantRepository.findAll()).thenReturn(etudiantList);
+
+        List<Etudiant> etudiantListResult = etudiantService.retrieveAllEtudiants();
+
+        assertEquals(1, etudiantListResult.size()); // Assuming one Etudiant was added in the setup
+    }
+}
